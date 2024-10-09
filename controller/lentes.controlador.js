246cc3471b117c4e4aa70952c62653493038d8f0
Bndamer <35323956@ifts24.edu.ar>
2024-10-09 17:35:54 -0300

    Actualizacion

--- controller/lentes.controlador.js
+++ controller/lentes.controlador.js
@@ -1,7 +1,7 @@
 //controlador del modulo//
 const db =require("../db/db");
 
-//metodo get//
+//METODO GET//
 
 //para todas las lentes
 const allLens =(req,res) =>{
@@ -14,7 +14,91 @@ const allLens =(req,res) =>{
     });
 };
 
+
+
+//para un item o lente
+const showLens = (req,res) => {
+    const{id} = req.params;
+    const sql="SELECT * FROM lentes WHERE id = ?";
+    db.query(sql,[id],(error,rows )=> {
+        console.log(rows);
+        if(error){
+            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
+        }
+        if(rows.length ==0){
+            return res.status(400).send({error :"ERROR: No existe el lente requerido"});
+        };
+        res.json(rows[0]); //me muestra el elemento en la posucion cero si existe
+    });
+
+};
+
+//________________________//
+
+//METODO POST///
+const storeLens = (req, res) => {
+    const {modelo,tipo, apertura_min,apertura_max,distancia_focal_mm,marca_id,precio} = req.body;
+    const sql = "INSERT INTO lentes (modelo,tipo, apertura_min,apertura_max,distancia_focal_mm,marca_id,precio) VALUES (?,?,?,?,?,?,?)";
+    db.query(sql,[modelo,tipo, apertura_min,apertura_max,distancia_focal_mm,marca_id,precio], (error, result) => {
+        console.log(result);
+        if(error){
+            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
+        }
+        const lente = {...req.body, id: result.insertId}; // ... reconstruir el objeto del body
+        res.status(201).json(lente); // muestra creado con exito el elemento
+    });     
+
+};
+
+
+//// METODO PUT  ////
+const updateLens = (req, res) => {
+    const {id} = req.params;
+    const {modelo,tipo, apertura_min,apertura_max,distancia_focal_mm,marca_id,precio} = req.body;
+    const sql ="UPDATE lentes SET modelo = ?, tipo = ?, apertura_min = ?, apertura_max = ?, distancia_focal_mm = ? , marca_id = ? , precio = ?  WHERE id = ?";
+    db.query(sql,[modelo,tipo, apertura_min,apertura_max,distancia_focal_mm,marca_id,precio,id], (error, result) => {
+        console.log(result);
+        if(error){
+            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
+        }
+        if(result.affectedRows == 0){
+            return res.status(404).send({error : "ERROR: La lente a modificar no existe"});
+        };
+        
+        const lente = {...req.body, ...req.params}; // ... reconstruir el objeto del body
+
+        res.json(lente); // mostrar el elemento que existe
+    });     
+};
+
+
+//// METODO DELETE ////
+const destroyLens = (req, res) => {
+    const {id} = req.params;
+    const sql = "DELETE FROM lentes WHERE id = ?";
+    db.query(sql,[id], (error, result) => {
+        console.log(result);
+        if(error){
+            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
+        }
+        if(result.affectedRows == 0){
+            return res.status(404).send({error : "ERROR: La lente a borrar no existe"});
+        };
+        res.json({mesaje : "Lente Eliminada"});
+    }); 
+};
+
+
+
+
+
+
+
 //exportar todas las funciones del modulo
 module.exports={
     allLens,
+    showLens,
+    storeLens,
+    updateLens,
+    destroyLens
 };
--- routers/lentes.router.js
+++ routers/lentes.router.js
@@ -4,9 +4,23 @@ const router= express.Router();
 
 const controller =require("../controller/lentes.controlador");
 
-//metodo get//
+//METODO GET//
 //para tooos los lentes//
 router.get('/',controller.allLens);
+
+
+//para un producto,item o lente en mi caso
+router.get('/:id',controller.showLens);
+
+//METODO POST//
+router.post('/',controller.storeLens);
+
+//// METODO PUT  ////
+router.put('/:id', controller.updateLens);
+
+///// METODO DELETE ////
+router.delete('/:id', controller.destroyLens);
+
 
 //exportar las rutas,routers
 module.exports= router;
