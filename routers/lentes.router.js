4ba1d678f31e8f0a02bd56e92bcc44f39e714f96
Bndamer <35323956@ifts24.edu.ar>
2024-10-15 12:22:01 -0300

    actualizacion

--- routers/lentes.router.js
+++ routers/lentes.router.js
@@ -4,22 +4,43 @@ const router= express.Router();
 
 const controller =require("../controller/lentes.controlador");
 
+///MULTER///
+const multer=require("multer");
+const path =require("path");
+const storage =multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, 'uploads');  //esta carpeta debe existir en el proyecto raiz
+    },
+    filename: (req, file,cb) =>{
+        console.log(file);
+        cb(null, Date,now() + path.extname(file.originalname)); //segundos desde 1970
+    }
+});
+//const upload= multer({storage:"storage"}); //si son iguales simplemente lo puedeo escribir como
+const upload= multer({storage});
+
+
+
 //METODO GET//
 //para tooos los lentes//
 router.get('/',controller.allLens);
 
-
 //para un producto,item o lente en mi caso
 router.get('/:id',controller.showLens);
 
+
+
 //METODO POST//
-router.post('/',controller.storeLens);
+router.post('/', controller.storeLens);
+
 
 //// METODO PUT  ////
 router.put('/:id', controller.updateLens);
 
+
 ///// METODO DELETE ////
 router.delete('/:id', controller.destroyLens);
+
 
 
 //exportar las rutas,routers
